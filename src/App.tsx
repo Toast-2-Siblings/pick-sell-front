cc96875a0746f7e0affe89785368e93ccd51256f
jhphon0730 <eeung025@gmail.com>
2025-03-17 10:54:33 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-amber-50">
+			Hello Pick-Sell
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+
+// CSS
+import './index.css'
+
+// Components
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+	<App />
+)
